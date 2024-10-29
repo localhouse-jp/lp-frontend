d074421a1e4ec5d75e8a1d316eb616246e243baf
hirossan4049 <haruto405329@gmail.com>
2024-10-29 18:18:22 +0900

    一旦

--- src/app/_components/Home.tsx
+++ src/app/_components/Home.tsx
@@ -1,7 +1,14 @@
+
+import SuperTechEngineerHouse from "#/assets/SuperTechEngineerHouse.svg";
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="bg-gray-200 h-[80vh]">
-      <p className="text-xl text-center p-32">Home</p>
+    <div className="h-[80vh]">
+      {/* <img src='./assets/SuperTechEngineerHouse.svg' alt="SuperTechEngineerHouse" className="m-auto" /> */}
+      {/* <p className="text-xl text-center p-32">Home</p> */}
+      <Image src={SuperTechEngineerHouse.src} alt={""} width={512} height={16} className="sm:w-[70%] md:max-w-[520px] md:min-w-[320px] md:m-32 sm:m-16 xs:m-8" />
+
     </div>
   );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const Footer: FC = () => {
   return (
-    <footer className="text-gray-500 text-center p-2 text-sm">
+    <footer className="text-gray-500 text-center p-4 text-sm">
       <p>Copyright© LOCALHOUSE. All Rights Reserved.</p>
     </footer>
   );
