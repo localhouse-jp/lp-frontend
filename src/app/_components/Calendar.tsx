078b0f328020e166086e1750b1a5f09d4d053a71
hirossan4049 <haruto405329@gmail.com>
2024-10-29 18:18:22 +0900

    一旦

--- src/app/_components/Calendar.tsx
+++ src/app/_components/Calendar.tsx
@@ -1,7 +1,7 @@
 export default function Calendar() {
   return (
-    <div className="bg-[#f0f4f9] py-32">
-      <div className="m-auto md:w-[80%] sm:w-[90%] md:h-[820px] sm:h-[420px]">
+    <div className="bg-[#f0f4f9] pt-16 pb-32">
+      <div className="mx-auto my-16 md:w-[80%] sm:w-[90%] md:h-[820px] sm:h-[420px]">
         <p className="text-3xl px-4 py-8 font-bold">カレンダー</p>
         <iframe src="https://calendar.google.com/calendar/embed?src=classroom108557204569355481911%40group.calendar.google.com&ctz=Asia%2FTokyo" className="w-full h-full border-none"></iframe>
       </div>
