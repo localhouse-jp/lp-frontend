ae7401e0860d2dde8cfba590b1f8640f533bb8b5
hirossan4049 <haruto405329@gmail.com>
2024-10-31 18:29:17 +0900

    うげー

--- src/app/_components/Why.tsx
+++ src/app/_components/Why.tsx
@@ -1,10 +1,10 @@
 export default function Why() {
   return (
     <div className="flex flex-col lg:flex-row py-32 px-4 md:px-32 max-w-[1200px] md:mx-auto gap-12 justify-center">
-      <p className="font-bold min-w-[240px] text-2xl my-auto text-center">なぜ始めるのか？</p>
+      <p className="font-bold min-w-[240px] text-3xl my-auto text-center">なぜ始めるのか？</p>
       <div className="flex flex-col lg:max-w-[440px] max-w-full gap-4">
-        <p className="font-bold w-full text-xl">エンジニアが創造的な活動ができる場所を創りたい</p>
-        <p>エンジニアが技術を駆使して新しい価値を生み出すためには、自由に創造できる環境が必要です。<br />
+        <p className="font-bold w-full lg:text-2xl text-xl">エンジニアが創造的な活動ができる場所を創りたい</p>
+        <p className="lg:text-xl">エンジニアが技術を駆使して新しい価値を生み出すためには、自由に創造できる環境が必要です。<br />
           既存のコミュニティでは技術習得や情報共有が中心となることが多いですが、私たちは一歩進んで、エンジニアが自分のスキルやアイデアを思い切り活かし、挑戦できる場所を提供したいと考えています。<br />
           <br />
           私たちが目指すのは、誰もが気軽に集まり、自然にアイデアが広がり、技術と創造性が融合する空間です。時間や形式に縛られず、エンジニアが自分のペースで創造活動に取り組むことができる、オープンでフレンドリーな環境を作り上げます。ここでは、互いに刺激を与え合いながら共に成長したいと思っています。</p>
