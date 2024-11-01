11eea2d9b9ce7fb35339582d8f8596ba5c9eb23c
hirossan4049 <haruto405329@gmail.com>
2024-11-01 10:23:04 +0900

    一旦

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -1,7 +1,49 @@
+'use client'
+import { fetchAttachment } from "@/common/api/fetchAttachment";
+import { fetchProjects, Project } from "@/common/api/fetchProjects";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 export default function Projects() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      setProjects(await fetchProjects())
+    })();
+  }, [])
   return (
-    <div className="bg-gray-200 h-[80vh]">
+    <div className="h-[80vh] mx-auto">
       <p className="text-xl text-center p-32">Projects</p>
+
+      <div className="flex">
+        {projects.map((project, index) => (
+          <Item key={index} project={project} />
+        ))}
+      </div>
     </div>
   );
+}
+
+function Item({ project }: { project: Project }) {
+  const [image, setImage] = useState<string>('');
+  const [imageStatus, setImageStatus] = useState<'loading' | 'loaded' | 'error'>('loading');
+  useEffect(() => {
+    (async () => {
+      const image = await fetchAttachment(project.attachment?.[0])
+      setImage(image);
+      setImageStatus('loaded');
+    })();
+  }, [project.attachment])
+
+  return <Link className="flex flex-col flex-wrap gap-2 m-6 w-128 overflow-hidden" href={project.url} target="_blank">
+    <div className={`bg-gray-50 h-64 ${imageStatus === 'loading' && 'animate-pulse'}`}>
+      {imageStatus === 'loaded' &&
+        <Image src={image} alt={""} width={300} height={200} className="object-cover h-full" onError={() => {setImageStatus('error')}} />
+      }
+    </div>
+    <p className="font-bold text-xl w-128">{project.title}</p>
+    <p className="w-[300px]  text-ellipsis line-clamp-2 from-neutral-300">{project.description}</p>
+  </Link>
 }
---
+++ src/common/api/fetchProjects.ts
@@ -0,0 +1,31 @@
+'use client'
+
+
+export type Project = {
+  title: string;
+  description: string;
+  attachment: string[];
+  url: string;
+}
+
+export async function fetchProjects(): Promise<Project[]> {
+  const res = await fetch(process.env.WP_BASE_URL + "/?rest_route=/wp/v2/posts&categories=3", {
+    headers: {
+      "Content-Type": "application/json",
+      "CF-Access-Client-Id": process.env.CF_ACCESS_CLIENT_ID || '',
+      "CF-Access-Client-Secret": process.env.CF_ACCESS_CLIENT_SECRET || '',
+    }
+  });
+  const data = await res.json();
+
+  return data.map((project: any): Project => {
+    const parser = new DOMParser();
+    const descriptionDom = parser.parseFromString(project.content.rendered, "text/html");
+    return {
+      title: project.title.rendered,
+      description: descriptionDom.querySelector("p")?.textContent || "",
+      attachment: project._links["wp:attachment"]?.map((link: any) => link.href) || [],
+      url: project.link,
+    }
+  });
+}
