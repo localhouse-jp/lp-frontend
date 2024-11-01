1ba371d1735eed67301e8e2e96e5c4857851a9ec
hirossan4049 <haruto405329@gmail.com>
2024-11-01 18:33:06 +0900

    一旦

--- src/app/_components/Home.tsx
+++ src/app/_components/Home.tsx
@@ -12,13 +12,13 @@ export default function Home() {
         <Image src={LOCALHOUSE_Background.src} alt={""} width={512} height={16} className="sm:w-[70%] md:max-w-[820px] md:min-w-[320px] " />
       </div>
       <div className="p-8 md:p-12 flex justify-between flex-wrap">
-        <div className="flex flex-col md:gap-12 gap-4 mt-28 z-10">
+        <div className="flex flex-col md:gap-12 gap-4 md:mt-28 mt-20 z-10">
           <Image src={SuperTechEngineerHouse.src} alt={""} width={512} height={16} className="sm:w-[70%] md:max-w-[520px] md:min-w-[320px]" />
           <p>自由と創造性を追求する、学生ITエンジニアのためのオープンコミュニティ</p>
         </div>
 
 
-        <div className="flex flex-col text-right md:mt-28 mt-12 ml-auto z-10">
+        <div className="flex flex-col text-right md:mt-28 mt-20 ml-auto z-10">
           <p className="font-bold">大阪府東大阪市</p>
           <p className="font-bold">HIGASHI-OSAKA, OSAKA</p>
           <LinkButton href="https://localhouse.connpass.com" title="イベント情報を見る" />
