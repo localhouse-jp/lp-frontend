3ef09a70920ed88fb86d998e733b24a96625a5bf
xpadev <xpadev@gmail.com>
2024-11-02 23:09:05 +0900

    refactor: hero section

--- src/app/_components/Home.tsx
+++ src/app/_components/Home.tsx
@@ -1,24 +1,24 @@
 'use client'
-import LOCALHOUSE_Background from "#/assets/LOCALHOUSE_Background.svg";
-import SuperTechEngineerHouse from "#/assets/SuperTechEngineerHouse.svg";
 import Sample01 from "#/assets/sample01.png";
 import Image from 'next/image';
 import { LinkButton } from "./LinkButton";
+import {BackgroundText} from "@/components/BackgroundText";
+import {ForegroundText} from "@/app/_components/ForegroundText";
 
 export default function Home() {
   return (
     <div className="">
-      <div className="absolute z-0 m-12 overflow-hidden">
-        <Image src={LOCALHOUSE_Background.src} alt={""} width={512} height={16} className="sm:w-[70%] md:max-w-[820px] md:min-w-[320px] " />
+      <div className="absolute z-0 mx-4 lg:mx-10 overflow-hidden">
+        <BackgroundText/>
       </div>
-      <div className="p-8 md:p-12 flex justify-between flex-wrap">
-        <div className="flex flex-col md:gap-12 gap-4 md:mt-28 mt-20 z-10">
-          <Image src={SuperTechEngineerHouse.src} alt={""} width={512} height={16} className="sm:w-[70%] md:max-w-[520px] md:min-w-[320px]" />
-          <p>自由と創造性を追求する、学生ITエンジニアのためのオープンコミュニティ</p>
+      <div className="p-8 lg:p-12 flex flex-col md:flex-row md:justify-between gap-8">
+        <div className="flex flex-col lg:gap-12 gap-4 lg:mt-8 z-10">
+          <ForegroundText/>
+          <p>自由と創造性を追求する、<wbr/>学生ITエンジニアのための<wbr/>オープンコミュニティ</p>
         </div>
 
 
-        <div className="flex flex-col text-right md:mt-28 mt-20 ml-auto z-10">
+        <div className="flex flex-col text-right z-10 md:justify-center items-end flex-shrink-0">
           <p className="font-bold">大阪府東大阪市</p>
           <p className="font-bold">HIGASHI-OSAKA, OSAKA</p>
           <LinkButton href="https://localhouse.connpass.com" title="イベント情報を見る" />
