a6458b605675090c93b71f5f18dd5287f244fa2d
hirossan4049 <haruto405329@gmail.com>
2024-11-07 01:24:38 +0900

    news!

--- src/app/(top)/_components/News.tsx
+++ src/app/(top)/_components/News.tsx
@@ -1,6 +1,20 @@
+'use client'
 import TitlePin from "@/app/_components/TitlePin";
+import { fetchNews, News as NewsType } from "@/common/api/fetchNews";
+import { useEffect, useState } from "react";
 
 export default function News() {
+  const [news, setNews] = useState<NewsType[]>([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        setNews(await fetchNews())
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [])
+
   return (
     <div className="md:my-64 my-32 mx-auto max-w-[1200px] justify-center" id="news">
 
@@ -8,16 +22,16 @@ export default function News() {
         <TitlePin title="News" />
       </div>
       <p className="md:text-2xl text-xl px-4 py-4 font-bold md:ml-32">ニュース</p>
-
-      <Item title={"ローカルハウスを設立しました！"} date={"2024.11.4"} description={"よろしくお願いいたします！"} link={""} />
-
+      {news.map((news, index) => (
+        <Item key={index} title={news.title} description={news.description} date={news.date} link={news.url} />
+      ))}
     </div>
   );
 }
 
 type ItemProps = {
   title: string;
-  date: string;
+  date: Date;
   description: string;
   link: string;
 }
@@ -28,8 +42,9 @@ function Item(props: ItemProps) {
       <a
         href={props.link}
         className="p-2 w-full duration-500 hover:opacity-80 hover:scale-[99%]"
+        target="_blank"
       >
-        <p className="text-xs text-neutral-500">{props.date}</p>
+        <p className="text-xs text-neutral-500">{props.date.toLocaleDateString('sv-SE').replaceAll("-", ".")}</p>
         <p className="font-bold text-md">{props.title}</p>
         <p className="text-sm text-neutral-800">{props.description}</p>
       </a>
