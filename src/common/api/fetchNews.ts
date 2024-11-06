5e0b4a2bc81576654d668a0f4fc2075d5d2f52ba
hirossan4049 <haruto405329@gmail.com>
2024-11-07 01:24:38 +0900

    news!

--- src/common/api/fetchNews.ts
+++ src/common/api/fetchNews.ts
@@ -1,14 +1,14 @@
 'use client'
 
-export type Project = {
+export type News = {
   title: string;
   description: string;
-  attachment: string[];
+  date: Date;
   url: string;
 }
 
-export async function fetchProjects(): Promise<Project[]> {
-  const res = await fetch(process.env.NEXT_PUBLIC_WP_BASE_URL + "/?rest_route=/wp/v2/posts&per_page=3&categories=3", {
+export async function fetchNews(): Promise<News[]> {
+  const res = await fetch(process.env.NEXT_PUBLIC_WP_BASE_URL + "/?rest_route=/wp/v2/posts&per_page=5&categories=5", {
     headers: {
       "Content-Type": "application/json",
     }
@@ -16,14 +16,13 @@ export async function fetchProjects(): Promise<Project[]> {
   const data = await res.json();
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return data.map((project: any): Project => {
+  return data.map((project: any): News => {
     const parser = new DOMParser();
     const descriptionDom = parser.parseFromString(project.content.rendered, "text/html");
     return {
       title: project.title.rendered,
       description: descriptionDom.querySelector("p")?.textContent || "",
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      attachment: project._links["wp:attachment"]?.map((link: any) => link.href) || [],
+      date: new Date(project.date),
       url: project.link,
     }
   });
