01c61a059289db11bbb9c254546e8b9ad5bbfcc6
hirossan4049 <haruto405329@gmail.com>
2025-02-21 12:07:48 +0900

    u

---
+++ components/layout/GoogleAnalytics.tsx
@@ -0,0 +1,18 @@
+'use client'
+import Script from "next/script";
+
+export default function GoogleAnalytics() {
+  return <>
+    <Script defer src={`https://www.googletagmanager.com/gtag/js?id=G-7P9CX9TVKZ`} strategy="afterInteractive" />
+    <Script id="ga" defer strategy="afterInteractive">
+      {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());    
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+          `}
+    </Script>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWK8KZ9P"
+      height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+  </>
+}
